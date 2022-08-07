b5a27e8ce7ce68dfc0aac2f34615c04ddcf7459e
Thiago Silva <silvathiago015@gmail.com>
2022-08-07 12:42:19 -0300

    refactor: clean project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,5 @@
-import { makeMovieApi } from 'main/factories/make-movie-api'
-import { useEffect } from 'react'
-
 const Home = () => {
-  useEffect(() => {
-    makeMovieApi()
-      .loadPopular(1)
-      .then((data) => console.log(data))
-  }, [])
-  return <h2>teste</h2>
+  return null
 }
 
 export default Home
