824e4c26f56286c91f93e6cad69ec2ea2d1a9dae
Thiago Silva <silvathiago015@gmail.com>
2022-08-24 10:12:12 -0300

    feat: footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,6 @@
+import * as S from './styles'
+
+const Footer = () => (
+  <S.Wrapper>2022 - Dados providos por themoviedb.org</S.Wrapper>
+)
+export { Footer }
---
+++ src/components/footer/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.footer`
+  margin-top: 4.8rem;
+  padding: 3.2rem;
+  text-align: center;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  /* identical to box height, or 150% */
+
+  letter-spacing: 0.02em;
+
+  color: #ffffff;
+`
