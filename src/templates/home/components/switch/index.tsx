7798dbc2a0aca6e634e8e1a6c08ff4c3f2775c38
Thiago Silva <silvathiago015@gmail.com>
2022-08-24 10:11:55 -0300

    feat: switch component

--- src/templates/home/components/switch/index.tsx
+++ src/templates/home/components/switch/index.tsx
@@ -1,15 +1,32 @@
 import * as S from './styles'
 
 type SwitchProps = {
+  items: {
+    label: string
+    value: string
+  }[]
   value: string
+  onChange: (value: string) => void
 }
 
-export const Switch = ({ value }: SwitchProps) => {
+export const Switch = ({ value, onChange, items }: SwitchProps) => {
+  if (!items || items.length === 0) throw new Error('items cannot be empty')
+
+  const getActiveIndex = () => items.findIndex((item) => item.value === value)
   return (
     <S.Switch>
-      <S.SwitchItem>Todos</S.SwitchItem>
-      <S.SwitchItem>Filmes</S.SwitchItem>
-      <S.SwitchItem>Séries</S.SwitchItem>
+      <S.Wrapper activeIndex={getActiveIndex()} totalItems={items.length}>
+        {items.map((item) => (
+          <S.SwitchItem
+            active={value === item.value}
+            key={item.value}
+            onClick={() => onChange(item.value)}
+            onFocus={() => onChange(item.value)}
+          >
+            {item.label}
+          </S.SwitchItem>
+        ))}
+      </S.Wrapper>
     </S.Switch>
   )
 }
--- src/templates/home/components/switch/styles.ts
+++ src/templates/home/components/switch/styles.ts
@@ -1,5 +1,58 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+type SwitchProps = {
+  totalItems: number
+  activeIndex: number
+}
 
-export const Switch = styled.div``
+export const Wrapper = styled.div<SwitchProps>`
+  ${({ theme, totalItems, activeIndex }) => css`
+    display: grid;
+    grid-template-columns: repeat(${totalItems}, 1fr);
+    align-items: center;
+    justify-content: center;
+    position: relative;
 
-export const SwitchItem = styled.button``
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: calc(((100% / ${totalItems}) * ${activeIndex}));
+      background-color: black;
+      width: calc((100%) / ${totalItems});
+      height: calc(100%);
+      background-color: ${theme.colors.primary[500]};
+      z-index: ${theme.layers.base};
+      transition: all 550ms ease;
+      border-radius: 0.8rem;
+    }
+  `}
+`
+export const Switch = styled.div`
+  width: fit-content;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 0.8rem;
+  border-radius: 1.2rem;
+`
+type SwitchItemProps = {
+  active: boolean
+}
+
+export const SwitchItem = styled.button<SwitchItemProps>`
+  ${({ theme, active }) => css`
+    cursor: pointer;
+    background: none;
+    border: none;
+    padding: 0.8rem 3.2rem;
+    color: ${theme.colors.dark[300]};
+    font-family: ${theme.font.family.secondary};
+    font-weight: 600;
+    font-size: 1.6rem;
+    letter-spacing: 0.02em;
+    transition: all 250ms ease;
+    z-index: ${theme.layers.menu};
+    ${active &&
+    css`
+      color: ${theme.colors.primary[50]};
+    `}
+  `}
+`
